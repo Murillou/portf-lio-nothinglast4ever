4319e197a16c4da7ae1482ce1257ca365c5b376a
Murillou <murillovinicius13@hotmail.com>
2024-07-13 13:00:08 -0300

    [feat] componente home criado e estilizado

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import styles from './Home.module.css';
+
+export function Home() {
+  return (
+    <main>
+      <div className={styles.star}></div>
+      <h1 className={styles.title}>NOTHING LASTS FOREVER</h1>
+      <p>stars</p>
+      <h4>BRAZILIAN GRAPHIC DESIGN</h4>
+    </main>
+  );
+}
