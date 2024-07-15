f6c48b7acaa17d0365064c64310ede63831afc2e
Murillou <murillovinicius13@hotmail.com>
2024-07-15 15:55:52 -0300

    [feat] modal adicionado ao clicar nos itens do menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,17 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import posterArtImg from '../../assets/POSTER-BOTAO.png';
 import merchDesignImg from '../../assets/MERCH-BOTAO.png';
 import albumArtworkImg from '../../assets/BOTOES.png';
-import abooutMeImg from '../../assets/ABOUT-BOTAO.png';
+import aboutMeImg from '../../assets/ABOUT-BOTAO.png';
 import contactImg from '../../assets/CONTACT-BOTAO.png';
-
-import { useState } from 'react';
 import styles from './Header.module.css';
+
+Modal.setAppElement('#root');
 
 export function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  const [activeModal, setActiveModal] = useState<string | null>(null);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -16,6 +19,15 @@ export function Header() {
 
   const closeMenu = () => {
     setIsOpen(false);
+  };
+
+  const openModal = (modalName: string) => {
+    setActiveModal(modalName);
+    closeMenu();
+  };
+
+  const closeModal = () => {
+    setActiveModal(null);
   };
 
   return (
@@ -34,35 +46,100 @@ export function Header() {
           <button className={styles.closeButton} onClick={closeMenu}>
             &times;
           </button>
-          <li>
+          <li onClick={() => openModal('posterArt')}>
             <img
               src={posterArtImg}
               alt="Poster Art"
               className={styles.imgPosterArt}
             />
           </li>
-          <li>
+          <li onClick={() => openModal('merchDesign')}>
             <img
               src={merchDesignImg}
               alt="Merch Design"
               className={styles.imgMerchDesign}
             />
           </li>
-          <li>
+          <li onClick={() => openModal('albumArtwork')}>
             <img
               src={albumArtworkImg}
               alt="Album Artwork"
               className={styles.imgAlbumArtwork}
             />
           </li>
-          <li>
-            <img src={abooutMeImg} alt="About Me" className={styles.imgAbout} />
+          <li onClick={() => openModal('aboutMe')}>
+            <img src={aboutMeImg} alt="About Me" className={styles.imgAbout} />
           </li>
-          <li>
+          <li onClick={() => openModal('contact')}>
             <img src={contactImg} alt="Contact" className={styles.imgContact} />
           </li>
         </ul>
       </nav>
+
+      <Modal
+        isOpen={activeModal === 'posterArt'}
+        onRequestClose={closeModal}
+        className={styles.modalContent}
+        overlayClassName={styles.modalOverlay}
+      >
+        <button className={styles.modalCloseButton} onClick={closeModal}>
+          &times;
+        </button>
+        <h2>POSTER ART</h2>
+        <p>Lorem Impsum</p>
+      </Modal>
+
+      <Modal
+        isOpen={activeModal === 'merchDesign'}
+        onRequestClose={closeModal}
+        className={styles.modalContent}
+        overlayClassName={styles.modalOverlay}
+      >
+        <button className={styles.modalCloseButton} onClick={closeModal}>
+          &times;
+        </button>
+        <h2>MERCH DESIGN</h2>
+        <p>Lorem Impsum</p>
+      </Modal>
+
+      <Modal
+        isOpen={activeModal === 'albumArtwork'}
+        onRequestClose={closeModal}
+        className={styles.modalContent}
+        overlayClassName={styles.modalOverlay}
+      >
+        <button className={styles.modalCloseButton} onClick={closeModal}>
+          &times;
+        </button>
+        <h2>ALBUM ARTWORK</h2>
+        <p>Lorem Impsum</p>
+      </Modal>
+
+      <Modal
+        isOpen={activeModal === 'aboutMe'}
+        onRequestClose={closeModal}
+        className={styles.modalContent}
+        overlayClassName={styles.modalOverlay}
+      >
+        <button className={styles.modalCloseButton} onClick={closeModal}>
+          &times;
+        </button>
+        <h2>ABOUT ME</h2>
+        <p>Lorem Impsum</p>
+      </Modal>
+
+      <Modal
+        isOpen={activeModal === 'contact'}
+        onRequestClose={closeModal}
+        className={styles.modalContent}
+        overlayClassName={styles.modalOverlay}
+      >
+        <button className={styles.modalCloseButton} onClick={closeModal}>
+          &times;
+        </button>
+        <h2>CONTACT</h2>
+        <p>Lorem Impsum</p>
+      </Modal>
     </header>
   );
 }
